8005cf6988e451b592d163cc51447c5227f5b8a4
Lucas Oliveira <lucas.oliveira@ifsp.edu.br>
2018-02-26 18:05:37 -0300

    Aula 2, exercício 1: aprendendo a realizar leitura e escrita do console, convertendo as informações de string para números com o método Parse.

---
+++ Aula_02_Ex_01/Aula_02_Ex_01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_02_Ex_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double largura, comprimento, preco;
+            Console.Write("Largura:");
+            string valor = Console.ReadLine();
+            largura = double.Parse(valor);
+            Console.Write("Comprimento:");
+            comprimento = double.Parse(Console.ReadLine());
+            Console.Write("Preço:");
+            preco = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("O tamanho da sala é:" + (largura * comprimento));
+            Console.WriteLine("O preco total é {0} reais.", largura * comprimento * preco);
+
+            Console.Read();
+        }
+    }
+}
